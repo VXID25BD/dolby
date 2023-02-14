7c7cc6e9142be66c8eb0d9e250b88cf61a9133bb
Brilliant-1f48e <brilliant1f48e@gmail.com>
2023-02-14 20:34:06 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const PugPlugin = require("pug-plugin");
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src", "views", "index", "index.pug"),
+    },
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        publicPath: '/',
+        filename: 'assets/js/[name].[contenthash:8].js'
+    },
+    plugins: [
+        new PugPlugin({
+            pretty: true,
+            js: {
+                filename: "assets/js/[name].[contenthash:8].js"
+            },
+            css: {
+                filename: "assets/js/[name].[contenthash:8].js"
+            }
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.pug$/,
+            loader: PugPlugin.loader
+          },
+          {
+            test: /\.(css|sass|scss)$/,
+            use: ['css-loader', 'sass-loader']
+          },
+          {
+            test: /\.(png|jpg|jpeg|ico)/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'assets/img/[name].[hash:8][ext]'
+            }
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+            type: 'asset/resource',
+            generator: {
+              filename: 'assets/fonts/[name][ext][query]'
+            }
+          }
+        ]
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist')
+        },
+        watchFiles: {
+          paths: ['src/**/*.*', 'assets/scss/**/*.*'],
+          options: {
+            usePolling: true
+          }
+        }
+      },
+      stats: 'errors-only'
+
+}
