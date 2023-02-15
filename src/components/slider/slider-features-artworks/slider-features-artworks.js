dd022d0fd83743140a0e2fa3163e83f922b93c29
Brilliant-1f48e <brilliant1f48e@gmail.com>
2023-02-16 03:39:07 +0700

    finish component recent-activity and slider-recent-activity

--- src/components/slider/slider-features-artworks/slider-features-artworks.js
+++
@@ -1,7 +0,0 @@
-import Swiper from 'swiper';
-
-const sliderFeaturesArtworks = new Swiper(".slider-features-artworks", {
-    slidesPerView: 1,
-});
-
-export default sliderFeaturesArtworks;
--- src/views/index/main.js
+++ src/views/index/main.js
@@ -2,8 +2,19 @@
 const sliderFeaturesArtworks = new Swiper(".slider-features-artworks", {
     slidesPerView: 4,
     spaceBetween: 31,
+    watchOverflow: false,
     navigation: {
-        nextEl: '.slider__button-next',
-        prevEl: '.slider__button-prev',
+        nextEl: '.slider-features-artworks__button-next',
+        prevEl: '.slider-features-artworks__button-prev',
+      },
+});
+
+const sliderRecentActivity = new Swiper(".slider-recent-activity", {
+    slidesPerView: 3,
+    spaceBetween: 31,
+    watchOverflow: false,
+    navigation: {
+        nextEl: '.slider-recent-activity__button-next',
+        prevEl: '.slider-recent-activity__button-prev',
       },
 });
