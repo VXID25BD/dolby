3ee6e1ac1f6687155013e1c94b313787564c1621
Brilliant-1f48e <brilliant1f48e@gmail.com>
2023-02-16 03:04:27 +0700

    finish features-artworks and slider

--- src/views/index/main.js
+++ src/views/index/main.js
@@ -2,4 +2,8 @@
 const sliderFeaturesArtworks = new Swiper(".slider-features-artworks", {
     slidesPerView: 4,
     spaceBetween: 31,
+    navigation: {
+        nextEl: '.slider__button-next',
+        prevEl: '.slider__button-prev',
+      },
 });
